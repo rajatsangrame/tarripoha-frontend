a21adce3b766d9d8340a1299d273f7d84e7ab371
Rajat Sangrame <rajat.sangrame@gmail.com>
2025-02-03 03:56:11 +0530

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Layout from './layout/Layout';
@@ -23,7 +22,7 @@ const router = createBrowserRouter([
         Component: Layout,
         children: [
           {
-            path: '/',
+            path: '/home',
             Component: Home,
           },
           {
@@ -41,8 +40,3 @@ root.render(
     <RouterProvider router={router}/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
